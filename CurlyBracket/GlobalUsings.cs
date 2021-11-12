35e42ec50e7b52f62d8822b40363e26854f972f6
Cole Johnson <coleharrisjohnson@gmail.com>
2021-11-11 19:34:00 -0500

    Initial commit

---
+++ CurlyBracket/GlobalUsings.cs
@@ -0,0 +1,27 @@
+/* =============================================================================
+ * File:   GlobalUsings.cs
+ * Author: Cole Tobin
+ * =============================================================================
+ * <TODO: Purpose>
+ * =============================================================================
+ * Copyright (c) 2021 Cole Tobin
+ *
+ * This file is part of CurlyBracket.
+ *
+ * CurlyBracket is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the Free
+ *   Software Foundation, either version 3 of the License, or (at your option)
+ *   any later version.
+ *
+ * CurlyBracket is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *   more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ *   along with CurlyBracket. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+
+global using System;
+global using System.Diagnostics.Contracts;
---
+++ README.md
@@ -0,0 +1,22 @@
+# HyperText
+
+HyperText is an ambitious project where I create my own web browser. I will
+almost certainly never finish.
+
+## Subprojects
+
+HyperText is split into five components:
+
+- AngleBracket - The HTML engine
+- CurlyBracket - The JavaScript engine
+- HyperText - The actual browser application
+- FiberWire - Networking related stuff
+- StyleSheet - The CSS engine
+
+## License
+
+All "library" components (everything but HyperText) are licensed under the
+[GNU Lesser General Public License 3.0](https://www.gnu.org/licenses/lgpl-3.0.en.html)
+or later. HyperText itself is licensed under the
+[GNU General Public License 3.0](https://www.gnu.org/licenses/gpl-3.0.en.html)
+or later.
