5d3b447e6823935f80a798b0d3161f1982966b21
Cole Johnson <coleharrisjohnson@gmail.com>
2021-11-12 20:34:38 -0500

    [CP] Added global usings

---
+++ CodePoint/_GlobalUsing.cs
@@ -0,0 +1,30 @@
+/* =============================================================================
+ * File:   _GlobalUsing.cs
+ * Author: Cole Tobin
+ * =============================================================================
+ * Purpose:
+ *
+ * C# 10 adds "global using" directives that will be implicitly included in any
+ *   file in the project. This file contains these.
+ * =============================================================================
+ * Copyright (c) 2021 Cole Tobin
+ *
+ * This file is part of CodePoint.
+ *
+ * CodePoint is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU General Public License as published by the Free
+ *   Software Foundation, either version 3 of the License, or (at your option)
+ *   any later version.
+ *
+ * CodePoint is distributed in the hope that it will be useful, but WITHOUT ANY
+ *   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *   details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ *   CodePoint. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+
+global using System;
+global using System.Diagnostics.Contracts;
